cb99d2dc5d1019984c910a3795b225f45c9878d7
ASUS <1723109431@qq.com>
2019-03-20 19:47:42 +0800

    新增uni-app

---
+++ first-uni-app/pages/ucenter/ucenter.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="content">
+		<view>
+			<text class="title">{{title}}的个人中心</text>
+			<navigator url="setting" hover-class="navigator-hover">
+				<button type="primary">个人设置<tton>
+			</navigator>
+		<view>
+		<view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '26'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+	
+</style>
