3f4a23218373bf645ab1927122fb0e8b2ab8b02d
ASUS <1723109431@qq.com>
2019-03-27 10:29:51 +0800

    新增二级路由模板

--- vue-router-demo1/src/App.vue
+++ vue-router-demo1/src/App.vue
@@ -1,63 +1,66 @@
 <template>
   <div id="app">
-		<div class="header">
-			<router-link to="/" class="nav-item1"><img :src="title"></router-link>
-			<router-link to="/task" class="nav-item">任务中心</router-link>
-			<router-link to="/lib" class="nav-item">库管理</router-link>
-			<router-link to="/ucenter" class="nav-item">刘磊</router-link>
-		</div>
+    <div class="header">
+      <router-link to="/" class="nav-item1">
+        <img :src="title">
+      </router-link>
+      <router-link to="/task" class="nav-item">任务中心</router-link>
+      <router-link to="/lib" class="nav-item">库管理</router-link>
+      <router-link to="/ucenter" class="nav-item">刘磊</router-link>
+    </div>
     <div class="container">
-			<router-view></router-view>
-		</div>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-		data() {
-			return {
-				title: 'https://static.cdn.oss.mosoteach.cn/mosoteach2/common/images/logo.png'
-			}
-		},
+  name: "App",
+  data() {
+    return {
+      title:
+        "https://static.cdn.oss.mosoteach.cn/mosoteach2/common/images/logo.png"
+    };
+  }
 };
 </script>
 
 <style>
-	#app{
-		background-color: rgb(244,244,244);
-	}
-.header{
-		display: flex;
-		height: 100px;
-		background-color: #fff;
-		font-size: 20px;
-		padding-left: 120px;
-		align-items: center;
+#app {
+  background-color: rgb(244, 244, 244);
 }
-.header img{
-	height: 150%;
-	width: 150%;
+.header {
+  display: flex;
+  height: 100px;
+  background-color: #fff;
+  font-size: 20px;
+  padding-left: 120px;
+  align-items: center;
 }
-.nav-item1{
-	padding-left: 100px;
-	width: 100px;
-	margin-right: 800px;
+.header img {
+  height: 150%;
+  width: 150%;
 }
-.nav-item{
-	width: 100px;
-	margin-right: 20px;
-	padding-bottom: 10px;
+.nav-item1 {
+  padding-left: 100px;
+  width: 100px;
+  margin-right: 800px;
+}
+.nav-item {
+  width: 100px;
+  margin-right: 20px;
+  padding-bottom: 10px;
 }
 .container {
-	width: 80%;
-	margin: 0 auto;
+  width: 80%;
+  margin: 0 auto;
 }
-a{
-	text-decoration: none;
-	color: #aaa;
+a {
+  text-decoration: none;
+  color: #aaa;
 }
-a:hover{
-	color: rgb(2,165,218);
+a:hover {
+  color: rgb(2, 165, 218);
 }
 </style>
---
+++ vue-router-template/src/components/Lib.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="container">
+		<div class="side-bar">
+			<router-link to="/lib_group">库分组</router-link>
+		</div>
+		<div class="top-bar">
+			<router-link to="/lib_exercises">题库</router-link>
+			<router-link to="/lib_resource">资源库</router-link>
+			<router-link to="/lib_activity">活动库</router-link>
+		</div>
+		<div class="content"><router-view /></div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Lib',
+	data() {
+		return {
+			
+		};
+	}
+};
+</script>
+
+<style>
+.container {
+		display: flex;
+		width: 90%;
+		margin: 0 auto;
+		
+	}
+.side-bar{
+	flex: 1 1 20%;
+	flex-direction: column;
+	background-color: #fff;
+}
+.top-bar{
+	flex: 1 1 70%;
+	padding-bottom: 20px;
+	}
+</style>
